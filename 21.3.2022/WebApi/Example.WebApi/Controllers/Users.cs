6b74bd1ac2e74c212dadebb87ff97c5a12adf9b8
Fuma <franjo.jumic@gmail.com>
2022-03-21 13:42:19 +0100

    WebApi commit

---
+++ 21.3.2022/WebApi/Example.WebApi/Controllers/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.WebApi.Controllers
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
