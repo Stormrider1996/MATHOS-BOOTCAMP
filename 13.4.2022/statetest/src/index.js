cb1c1141c11627fb0e0a8ee6fa4430bb86c5626e
Fuma <franjo.jumic@gmail.com>
2022-04-14 10:39:50 +0200

    class state bug

--- 13.4.2022/statetest/src/index.js
+++ 13.4.2022/statetest/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./style.css"
 function App() {
-    const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+    /*const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
     
     function addItem() {
         const newThingText = `Thing ${things.length + 1}`
@@ -17,7 +17,34 @@ function App() {
             {thingsElements}
         </div>
     )
+    */
+   
+}
+class Thing extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {things: ["Thing 1", "Thing 2"]};
+
+        this.addItem = this.addItem.bind(this);
+    
+    }
+    
+
+    addItem(){
+        const newThingText = `Thing ${this.state.things.length + 1}`;
+        this.setState((prevState) => [...prevState, newThingText]);
+    }
+
+    render(){
+        const thingsElements = this.state.things.map(thing => <p key={thing}>{thing}</p>)    
+        return (
+            <div>
+                <button onClick={this.addItem}>Add Item</button>
+                {thingsElements}
+            </div>
+        );
+    }
+
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<Thing />, document.getElementById('root'));
