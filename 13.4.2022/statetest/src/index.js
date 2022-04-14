14784d7a1820733c1321c2d1437179f0a9347180
Fuma <franjo.jumic@gmail.com>
2022-04-14 12:39:37 +0200

    dovrsena klas komponenta za primjer stateova

--- 13.4.2022/statetest/src/index.js
+++ 13.4.2022/statetest/src/index.js
@@ -1,50 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./style.css"
-function App() {
-    /*const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+
+// Primjer state-ova u funkcijskoj komponenti
+
+// // function App() {
+// //     /*const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
     
-    function addItem() {
-        const newThingText = `Thing ${things.length + 1}`
-        setThings(prevState => [...prevState, newThingText])
-    }
+// //     function addItem() {
+// //         const newThingText = `Thing ${things.length + 1}`
+// //         setThings(prevState => [...prevState, newThingText])
+// //     }
     
-    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+// //     const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
     
-    return (
-        <div>
-            <button onClick={addItem}>Add Item</button>
-            {thingsElements}
-        </div>
-    )
-    */
+// //     return (
+// //         <div>
+// //             <button onClick={addItem}>Add Item</button>
+// //             {thingsElements}
+// //         </div>
+// //     )
+// //     */
    
-}
-class Thing extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {things: ["Thing 1", "Thing 2"]};
+// }
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-        this.addItem = this.addItem.bind(this);
-    
-    }
-    
+// Primjer state-ova u class komponenti
 
-    addItem(){
-        const newThingText = `Thing ${this.state.things.length + 1}`;
-        this.setState((prevState) => [...prevState, newThingText]);
-    }
+class Thing extends React.Component {
+  constructor(props) {
+    super(props);
 
-    render(){
-        const thingsElements = this.state.things.map(thing => <p key={thing}>{thing}</p>)    
+    this.state = {
+      things: ["Thing 1", "Thing 2"]
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+
+    addItem() {
+        this.setState((prevState) => { 
+        return {
+        things: [...prevState.things, `Thing ${this.state.things.length + 1}`]
+      };
+    });
+  }
+
+    render() {
+        const thingsElements = this.state.things.map(thing => <p key={thing}>{thing}</p>)
+
         return (
             <div>
                 <button onClick={this.addItem}>Add Item</button>
-                {thingsElements}
+                {thingsElements}             
             </div>
         );
     }
-
 }
 
-ReactDOM.render(<Thing />, document.getElementById('root'));
+ReactDOM.render(<Thing />, document.getElementById("root"));
+
+
